92f49341bf32d1133ffc230769a48028a8eaeeaa
sarath67 <sarath.s2022cse@sece.ac.in>
2025-04-18 14:27:29 +0530

    e.e

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
